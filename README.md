34a208e27efa1c2a822e8ed6dc494655df624feb
Matt McManus <mattmcmanus@gmail.com>
2013-06-19 17:29:50 -0400

    Automatically generate help and run subcommands in local bin folder

--- README.md
+++ README.md
@@ -1,4 +1,79 @@
-node-helmsman
-=============
+# node-helmsman
 
 Easily make command line interfaces using git style subcommands
+
+Say your `bin/` folder includes the following files:
+
+* `git`
+* `git-status`
+* `git-commit`
+* `git-revert`
+* ...
+
+Wouldn't it be nice if:
+
+* Running `git --help` automatically generated help output, telling you all the subcommands that are available to you?
+* Running `git status` automatically runs the `git-status` file, passing all the arguments & options?
+* The subcommand (`git-status`) could use whatever option parsing library you wanted (commander or optimist)?
+
+Super! Helmsman is here to help!
+
+## Installation & Setup
+
+In your command line application folder:
+
+```
+npm install helmsman --save
+```
+
+### Setting up your main executable
+
+In your main executable (eq: `git`), add `helmsman`:
+
+```javascript
+var helmsman = require('helmsman');
+
+helmsman().parse();
+```
+
+Want to add custom help before the generated help?
+
+```javascript
+var helmsman = require('helmsman');
+
+var cli = helmsman()
+
+cli.on('--help', function(){
+  console.log('EXTRA HELPFUL!');
+});
+
+cli.parse();
+```
+
+### Setting up your sub-commands
+
+For your sub-executables to work with `helmsman` you need to do two things: 1. Expose metadata about the task, like its description and 2. Make sure the meat & pototoes of the script only runs when it's directly called
+
+To expose the metadata simply `exports.command`
+
+```javascript
+exports.command = {
+  description: 'Show current worker counts and their pids'
+};
+```
+
+Then make sure it only runs when it's directly called:
+
+```javascript
+if (require.main === module) {
+  // Parse options and run the magic
+}
+```
+
+## API
+
+...
+
+## TODO
+
+* [Allow for automatically including npm installed libraries](https://github.com/mattmcmanus/node-helmsman/issues/2)
---
+++ helmsman.js
@@ -0,0 +1,138 @@
+'use strict';
+
+/*!
+ * Module dependencies.
+ */
+var util = require('util');
+var events = require("events");
+var path = require('path');
+var glob = require('glob');
+var colors = require('colors');
+var spawn = require('child_process').spawn;
+
+/**
+ * Exports
+ */
+module.exports = exports = helmsman;
+exports.Helmsman = Helmsman;
+
+
+function err(mes) {
+  util.error(mes.red);
+  process.exit(1);
+}
+
+/**
+ * The Helmsman constructor
+ *
+ * Options:
+ *
+ *   * prefix: The prefix of the script files. eg: 'git-''
+ *
+ * @param  {object} options   config
+ */
+function Helmsman(options){
+  events.EventEmitter.call(this);
+
+  if (!options) { options = {}; }
+
+  this.localFolder = path.dirname(module.parent.filename);
+
+  this.prefix = options.prefix || path.basename(process.argv[1]);
+  this.availableCommands = {};
+  this.commandMaxLength = 0; //For printing help later
+
+  // Add a dash to the end if none was provided
+  if (this.prefix.substr(-1) !== '-') {
+    this.prefix += '-';
+  }
+}
+
+util.inherits(Helmsman, events.EventEmitter);
+
+
+/**
+ * Simplify creating a new Helmsman Object
+ *
+ * Example:
+ *
+ *   var helmsman = require('helmsman');
+ *
+ *   var cli = helmsman();
+ *   cli.parse();
+ * 
+ * @param  {Object} options   Contructor options
+ * @return {Helmsman}         A new helmsman
+ */
+function helmsman(options){
+  return new Helmsman(options);
+}
+
+
+/**
+ * GO!
+ */
+Helmsman.prototype.parse = function(argv){
+  var self = this;
+
+  argv = argv || process.argv; // If no arguments are passed, assume process.argv
+
+  var args = argv.slice(2);
+
+  // Local files in files in the /bin folder for an application
+  var localFiles = glob.sync(self.prefix+"*", {cwd: self.localFolder});
+
+  localFiles.forEach(function(file){
+    // Figure out the longest command name for printing --help
+    if (file.length > self.commandMaxLength) { self.commandMaxLength = file.length; }
+    
+    var commandData = require(path.join(self.localFolder, file)).command;
+    self.availableCommands[file.substr(self.prefix.length)] = commandData;
+  });
+
+  // Much of the following heavily inspired or simply taken from component/bin
+  // https://github.com/component/component/blob/master/bin/component
+  
+  // Print the command list if --help is called
+  if (args[0] === '--help' || !args.length) {
+    self.emit('--help');
+    return self.showHelp();
+  }
+
+  var cmd = args.shift();
+
+  if (!~Object.keys(self.availableCommands).indexOf(cmd)) {
+    err(util.format('There is no "%s" command', cmd));
+  }
+
+  var bin = path.join(self.localFolder, self.prefix + cmd);
+  var subcommand = spawn(bin, args, { stdio: 'inherit' });
+
+  subcommand.on('close', function(code){
+    process.exit(code);
+  });
+};
+
+
+/**
+ * Show the help
+ */
+Helmsman.prototype.showHelp = function(){
+  var self = this;
+  
+  console.log('');
+  console.log('Commands:');
+  console.log('');
+  
+  for (var command in self.availableCommands) {
+    var prettyCommand = command;
+    // Pad spaces at the end of each command so help descriptions line up
+    for (var i = 0; i < self.commandMaxLength-command.length; i++) {
+      prettyCommand+=' ';
+    }
+    // console.log(self.availableCommands[command]);
+    console.log('   %s     %s', prettyCommand, self.availableCommands[command].description);
+  }
+
+  process.exit();
+};
