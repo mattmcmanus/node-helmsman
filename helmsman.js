04a6cf84652cf5afc50755e52fe2362b2b3e549a
Beau Gunderson <beau@beaugunderson.com>
2014-12-12 09:41:23 -0800

    Support specifying metadata function, cleanup

--- helmsman.js
+++ helmsman.js
@@ -15,13 +15,22 @@ var domain = require('domain').create();
 
 require('colors');
 
-/**
- * Exports
- */
 
-module.exports = exports = helmsman;
+function defaultFillCommandData(commandData, file, extension) {
+  var data;
 
-exports.Helmsman = Helmsman;
+  try {
+    data = require(file).command || {};
+  } catch (e) {
+    // If it's JavaScript then return the error
+    if (extension === '.js') {
+      data = {failedRequire: e};
+    }
+  }
+
+  return _.merge(commandData, data);
+}
+
 
 /**
  * The Helmsman constructor
@@ -43,8 +52,15 @@ function Helmsman(options){
   options = _.merge({
     usePath: false,
     metadata: {},
+    fillCommandData: defaultFillCommandData,
+    fallbackCommandData: true,
+    ignoreRequireFail: true,
     nodePath: 'node'
   }, options);
+
+  this.fillCommandData = options.fillCommandData;
+  this.fallbackCommandData = options.fallbackCommandData;
+  this.ignoreRequireFail = options.ignoreRequireFail;
 
   this.nodePath = options.nodePath;
 
@@ -67,16 +83,13 @@ function Helmsman(options){
 
   this.availableCommands = {};
 
-  //For printing help later, 18 is help <sub-command>
-  this.commandMaxLength = 18;
-
   // Add a dash to the end if none was provided
   if (this.prefix.substr(-1) !== '-') {
     this.prefix += '-';
   }
 
   // Local files in files in the /bin folder for an application
-  this.localFiles = glob.sync(self.prefix + '*', { cwd: self.localDir })
+  this.localFiles = glob.sync(self.prefix + '*', {cwd: self.localDir})
     .map(function (file) {
       return path.join(self.localDir, file);
     });
@@ -96,40 +109,39 @@ function Helmsman(options){
     var extension = path.extname(file);
     var name = path.basename(file, extension).substr(self.prefix.length);
 
-    var commandData = {
-      description: ''
+    var defaultCommandData = {
+      name: name,
+      description: '',
+      arguments: '',
+      path: file
     };
+
+    var commandData;
 
     // Load the command data from the metadata option if present
     if (options.metadata[name]) {
-      commandData = _.merge(commandData, options.metadata[name]);
+      commandData = _.merge(defaultCommandData, options.metadata[name]);
     // Only try to get metadata from commands written in JavaScript
-    } else if (extension === '' || extension === '.js') {
-      try {
-        commandData = _.merge(commandData, require(file).command);
-      } catch (e) {
-        // If it is JavaScript and we fail to require it's an error
-        if (extension === '.js') {
-          util.error(util.format('The file "%s" did not load correctly: %s',
-            file, e).red);
+    } else if ((extension === '' || extension === '.js') &&
+               self.fillCommandData === defaultFillCommandData) {
+      commandData = self.fillCommandData(defaultCommandData, file, extension);
 
-          process.exit(1);
-        }
+      if (!self.ignoreRequireFail && commandData.failedRequire) {
+        util.error(util.format('The file "%s" did not load correctly: %s',
+          file, commandData.failedRequire).red);
+
+        process.exit(1);
+      }
+    } else if (self.fillCommandData !== defaultFillCommandData) {
+      commandData = self.fillCommandData(defaultCommandData, file, extension);
+
+      if (!commandData && self.fallbackCommandData) {
+        commandData = defaultFillCommandData(defaultCommandData,
+                                             file, extension);
       }
     }
 
-    commandData.path = file;
-
     self.availableCommands[name] = commandData;
-
-    var fullCommand = commandData.arguments ?
-      name + ' ' + commandData.arguments :
-      name;
-
-    // Figure out the longest command name for printing --help
-    if (fullCommand.length > self.commandMaxLength) {
-      self.commandMaxLength = name.length;
-    }
   });
 
   // help is always available!
@@ -155,7 +167,6 @@ util.inherits(Helmsman, events.EventEmitter);
  * @param  {Object} options   Contructor options
  * @return {Helmsman}         A new helmsman
  */
-
 function helmsman(options){
   return new Helmsman(options);
 }
@@ -229,7 +240,6 @@ Helmsman.prototype.getCommand = function(cmd, availableCommands){
  *
  * @param {[Object]} argv   The process arguments to parse. Defaults to process.argv
  */
-
 Helmsman.prototype.parse = function(argv){
   var self = this;
 
@@ -251,11 +261,12 @@ Helmsman.prototype.parse = function(argv){
 
   // Print the command list if --help is called
   if (args[0] === '--help' ||
-    !args.length ||
-    args[0][0] === '-' ||
-    (args[0] === 'help' && args.length === 1) ||
-    (self.getCommand(args[0]) === 'help' && args.length === 1)) {
+      !args.length ||
+      args[0][0] === '-' ||
+      (args[0] === 'help' && args.length === 1) ||
+      (self.getCommand(args[0]) === 'help' && args.length === 1)) {
     self.emit('--help');
+
     return self.showHelp();
   }
 
@@ -297,7 +308,8 @@ Helmsman.prototype.parse = function(argv){
       args.unshift(fullPath);
       fullPath = self.nodePath;
     }
-    var subcommand = spawn(fullPath, args, { stdio: 'inherit' });
+
+    var subcommand = spawn(fullPath, args, {stdio: 'inherit'});
 
     subcommand.on('close', function(code){
       process.exit(code);
@@ -309,39 +321,30 @@ Helmsman.prototype.parse = function(argv){
 /**
  * Show the help
  */
-
-Helmsman.prototype.showHelp = function(){
-  var self = this;
-  var fullCommands = [];
-
-  console.log('');
+Helmsman.prototype.showHelp = function () {
+  console.log();
   console.log('Commands:');
-  console.log('');
+  console.log();
 
-  for (var command in self.availableCommands) {
-    var fullCommand = command;
+  var strings = _.map(this.availableCommands, function (command) {
+    return command.name + ' ' + command.arguments;
+  });
 
-    if (self.availableCommands[command].arguments) {
-      fullCommand += ' ' + self.availableCommands[command].arguments;
-    }
+  var maxLength = _.max(_.pluck(strings, 'length'));
 
-    if (fullCommand.length > self.commandMaxLength) {
-      self.commandMaxLength = fullCommand.length;
-    }
+  var descriptions = _.pluck(this.availableCommands, 'description');
 
-    fullCommands.push([fullCommand, self.availableCommands[command].description]);
-  }
-
-  fullCommands.forEach(function(command){
-    var diff = (self.commandMaxLength - command[0].length);
-
-    // Pad spaces at the end of each command so help descriptions line up
-    for (var i = 0; i < diff; i++) {
-      command[0] += ' ';
-    }
-
-    console.log('   %s     %s', command[0], command[1]);
+  _.zip(strings, descriptions).forEach(function (c) {
+    console.log('   %s     %s', _s.rpad(c[0], maxLength), c[1]);
   });
 
   process.exit();
 };
+
+
+/**
+ * Exports
+ */
+module.exports = exports = helmsman;
+
+exports.Helmsman = Helmsman;
